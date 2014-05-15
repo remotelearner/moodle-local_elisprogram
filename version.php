a67190df8771e21d1f18c53d9771e940557b31bb
Brent Boghosian <brent.boghosian@remote-learner.net>
2014-05-15 14:12:09 -0400

    ELIS-8791: Migrate customized language strings for relocated ELIS Components

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->version = 2014030701;
 $plugin->release = '2.6.1.1 (Build: 20140415)';
 $plugin->dependencies = array(
     'enrol_elis' => 2014030700,
-    'local_eliscore' => 2014030700
+    'local_eliscore' => 2014030701
 );
