cfc7957bf25637a4f733130c4edf54177444cba7
Brent Boghosian <brent.boghosian@remote-learner.net>
2014-06-12 10:57:51 -0400

    ELIS-8862: Update version files release & build date

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014030703;
-$plugin->release = '2.6.1.2 (Build: 20140523)';
+$plugin->release = '2.6.1.3 (Build: 20140612)';
 $plugin->dependencies = array(
     'enrol_elis' => 2014030700,
     'local_eliscore' => 2014030701
