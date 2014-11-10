b9970d4066371fe6248146dd4ae7493863fb9970
Brent Boghosian <brent.boghosian@remote-learner.net>
2014-11-10 14:00:54 -0500

    ELIS-8989: Update version files: DB version (for main components), release & build dates.

--- version.php
+++ version.php
@@ -2,10 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014030705;
-$plugin->release = '2.6.4.1 (Build: 20140826)';
+$plugin->version = 2014030706;
+$plugin->release = '2.6.6.0 (Build: 20141110)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'enrol_elis' => 2014030700,
-    'local_eliscore' => 2014030701
+    'enrol_elis' => 2014030701,
+    'local_eliscore' => 2014030702
 );
