99b06f7a56ec268838f1a2e1643925b23fb42cd2
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-01-08 16:17:01 -0500

    ELIS-9031: Update elisprogram/enrol/userset/moodleprofile/db/events to use class observers.
    Conflicts:
    	local/elisprogram/enrol/userset/moodleprofile/version.php

--- enrol/userset/moodleprofile/version.php
+++ enrol/userset/moodleprofile/version.php
@@ -26,5 +26,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015010400;
-$plugin->release = '2.8.1.0 (Build: 20150104)';
+$plugin->version = 2015010401;
+$plugin->release = '2.8.2.0 (Build: 20150109)';
