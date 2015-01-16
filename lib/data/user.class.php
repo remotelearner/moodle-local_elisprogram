e9ca75610ef8519f365e854093bae1da659f4ce3
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-01-16 10:33:58 -0500

    ELIS-9037: Update deprecated textlib class to core_text.

--- lib/data/user.class.php
+++ lib/data/user.class.php
@@ -201,7 +201,7 @@ class user extends data_object_with_custom_fields {
             $data->username = $data->idnumber;
         }
         if (!empty($data->username)) {
-            $data->username = textlib::strtolower($data->username);
+            $data->username = core_text::strtolower($data->username);
         }
         $this->_load_data_from_record($data, true);
     }
