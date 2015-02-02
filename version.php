10fbdf3d776bc555964f1f41ac3d05a3d4554498
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-02-02 12:04:02 -0500

    ELIS-9056: Update elisprogram version release & build date.

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014030706;
-$plugin->release = '2.6.6.1 (Build: 20150104)';
+$plugin->release = '2.6.8.0 (Build: 20150202)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'enrol_elis' => 2014030701,
