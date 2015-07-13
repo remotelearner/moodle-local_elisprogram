df3fa2fc5d065a05955ed64197ed378714a8357b
Charles Verge <charles.verge@remote-learner.net>
2015-07-13 14:17:46 -0300

    ELIS-9201: Move track and program enrol dialogs to top of z-index

--- widgets/enrolment/js/widget.js
+++ widgets/enrolment/js/widget.js
@@ -345,6 +345,10 @@
             this.changestatus = function(e, action) {
                 e.preventDefault();
                 e.stopPropagation();
+                // Check if a dialog is already present.
+                if ($('.modaldialog').length) {
+                    return false;
+                }
                 // Add confirm dialog.
                 var height = 175;
                 var prompt = '<b>'+opts.lang['enrol_confirm_'+action]+'</b><br/>&nbsp;&nbsp;'+opts.lang.idnumber+': '+main.data.element_idnumber;
@@ -361,6 +365,7 @@
                 $('<div></div>').appendTo('body')
                     .html(prompt)
                     .dialog({
+                        dialogClass: 'modaldialog',
                         modal: true,
                         resizable: true,
                         height: height,
--- widgets/trackenrol/js/widget.js
+++ widgets/trackenrol/js/widget.js
@@ -310,6 +310,10 @@
             this.changestatus = function(e, action) {
                 e.preventDefault();
                 e.stopPropagation();
+                // Check if a dialog is already present.
+                if ($('.modaldialog').length) {
+                    return false;
+                }
                 // Add confirm dialog.
                 var height = 175;
                 var prompt = '<b>'+opts.lang['enrol_confirm_'+action]+'</b><br/>&nbsp;&nbsp;'+opts.lang.idnumber+': '+main.data.element_idnumber;
@@ -326,6 +330,7 @@
                 $('<div></div>').appendTo('body')
                     .html(prompt)
                     .dialog({
+                        dialogClass: 'modaldialog',
                         modal: true,
                         resizable: true,
                         height: height,
