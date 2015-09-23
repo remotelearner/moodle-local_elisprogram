df675139b763d73da835fb40eae741284d967960
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-09-23 11:20:33 -0400

    ELIS-9235: Update versions release & build date.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051104;
-$plugin->release = '2.9.1.0 (Build: 20150714)';
+$plugin->release = '2.9.2.0 (Build: 20150923)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'enrol_elis' => 2015051100,
--- widgets/trackenrol/version.php
+++ widgets/trackenrol/version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051102;
-$plugin->release = '2.9.1.0 (Build: 20150714)';
+$plugin->release = '2.9.2.0 (Build: 20150923)';
 $plugin->dependencies = array(
     'eliswidget_enrolment' => 2015051100
 );
