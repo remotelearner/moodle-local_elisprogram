51ec918da4919366aa8df9f7ea26c6763657d428
James McQuillan <james@pdyn.net>
2016-07-10 14:29:37 -0400

    ELIS-9414: Add incremental grade sync
    Incremental grade sync looks for changes before the sync process and
    syncs only new information. This can provide a drastically faster
    grade sync process than the previous version, which always synced all
    information. Incremental grade sync is enabled with a setting in the
    ELIS settings page. Disabled by default.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015051105;
+$plugin->version = 2015051106;
 $plugin->release = '2.9.5.0 (Build: 20160509)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
