77bfa4bf37383fb5f2c48bd41e122892189c3b22
Charles Verge <charles.verge@remote-learner.net>
2016-07-18 12:46:53 -0300

    ELIS-9418: Add autoenrol to usersettrack/trackuserset assign tab select
    The edit autoenrol feature on the assign tab requires the auto enrol data to be
    passed in the ajax request. If the auto enrol filter is removed from the filter list
    this does not happen. Added autoenrol field to select list by default.

--- lib/deepsight/lib/tables/usersettrack.table.php
+++ lib/deepsight/lib/tables/usersettrack.table.php
@@ -199,6 +199,24 @@ class deepsight_datatable_usersettrack_assigned extends deepsight_datatable_user
                            ON clsttrk.clusterid='.$this->usersetid.' AND clsttrk.trackid = element.id';
         return $joinsql;
     }
+
+    /**
+     * Gets an array of fields to include in the search SQL's SELECT clause. Adds in autoenrol to ensure auto enrol
+     * data is passed to deepsight.
+     *
+     * Pulls information from $this->fixed_columns, and each filter's get_select_fields() function.
+     *
+     * @uses deepsight_filter::get_select_fields();
+     * @uses deepsight_datatable_standard::$fixed_columns
+     * @uses deepsight_datatable_standard::$available_filters
+     * @param array $filters An Array of active filters to use to determine the needed select fields.
+     * @return array An array of fields for the SELECT clause.
+     */
+    protected function get_select_fields(array $filters) {
+        $selectfields = parent::get_select_fields($filters);
+        $selectfields[] = 'autoenrol AS autoenrol';
+        return  $selectfields;
+    }
 }
 
 /**
