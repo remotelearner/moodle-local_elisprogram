358b3d605c3d49ec67a7bc0472ff1cc8645bce61
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-08-25 12:52:43 -0400

    ELIS-9427: Correct clusterassignment::delete() method
    Corrected event from cluster_assigned to cluster_deassigned
    and moved delete call after event trigger in case reocrd required;
    also, updated PHPUnit test.

--- lib/data/clusterassignment.class.php
+++ lib/data/clusterassignment.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2013 Remote-Learner.net Inc (http://www.remote-learner.net)
+ * Copyright (C) 2008-2016 Remote-Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
  * @package    local_elisprogram
  * @author     Remote-Learner.net Inc
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
+ * @copyright  (C) 2008-2016 Remote Learner.net Inc (http://www.remote-learner.net)
  *
  */
 
@@ -71,18 +71,22 @@ class clusterassignment extends elis_data_object {
         array('validation_helper', 'is_unique_clusterid_userid_plugin')
     );
 
+    /**
+     * Delete method
+     * @return bool return status of deletion.
+     */
     public function delete() {
         $this->load(); // The object must be loaded before sending through to any event handlers -- ELIS-6567
-        $status = parent::delete();
-
         $eventdata = array(
             'context' => context_system::instance(),
             'other' => $this->to_array(true)
         );
-        $event = \local_elisprogram\event\cluster_assigned::create($eventdata);
+        $event = \local_elisprogram\event\cluster_deassigned::create($eventdata);
         $event->trigger();
+
+        $status = parent::delete();
         return $status;
-	}
+    }
 
 	public static function delete_for_user($id) {
     	global $DB;
