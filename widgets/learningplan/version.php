4957df8cbcf6396e8e6e12c2b24d927753d9c7b6
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-09-19 16:34:28 -0400

    ELIS-9429: Update versions releases.

--- widgets/learningplan/version.php
+++ widgets/learningplan/version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016052300;
-$plugin->release = '3.1.0.0 (Build: 20160720)';
+$plugin->release = '3.1.1.0 (Build: 20160919)';
 $plugin->component = 'eliswidget_learningplan';
 $plugin->dependencies = array(
     'eliswidget_common' => 2016052300
