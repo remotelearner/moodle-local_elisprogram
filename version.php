21c41e64528b04715a55c53ac04496b3f7925b6f
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-11-01 16:07:59 -0400

    ELIS-9435: Update versions release.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016052300;
-$plugin->release = '3.1.1.0 (Build: 20160919)';
+$plugin->release = '3.1.1.1 (Build: 20161101)';
 $plugin->component = 'local_elisprogram';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
