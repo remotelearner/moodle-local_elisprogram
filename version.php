11441b8b3362bace515be72b892a3f008fa83c52
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-11-15 11:54:39 -0500

    ELIS-9437: Update local_elisprogram release 2.7.16.0

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014082510;
-$plugin->release = '2.7.15.1 (Build: 20161101)';
+$plugin->release = '2.7.16.0 (Build: 20161115)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'enrol_elis' => 2014082501,
